31557d77ff03469def96eff0f5be60401f6b2263
makotoomori <makoto@loftwork.com>
2019-09-04 13:03:20 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,19 +9,7 @@ echo "Uploading files..."
 WDEFAULT_LOCAL_DIR=${LOCAL_DIR:-"."}
 WDEFAULT_REMOTE_DIR=${REMOTE_DIR:-"."}
 
-if [ $FTP_LOCAL_SERVER == "true" ]; then
-    echo "LFTP LOCAL_SERVER -> PROD_SERVER..."
-
-    #Entry LOCAL SERVER
-    #$FTP_LOCAL_USERNAME、$FTP_LOCAL_PASSWORD
-
-    #LOCAL SERVERからカレントへFTP
-
-    #find関数で.htaccessとwp-configを削除
-
-    #CurrentDir -> Production Server
-else
-    echo "LFTP LOCAL_DIR -> PROD_SERVER..."
+    echo "master branch -> PROD_SERVER..."
     set +e
     git config git-ftp.url $FTP_DEST
     git config git-ftp.user $FTP_USER
@@ -32,9 +20,5 @@ else
     if [ "$exit_code" -ne 0 ]; then
         git ftp push
     fi
-    #lftp $FTP_SERVER -u $FTP_USERNAME,$FTP_PASSWORD -e "set ftp:ssl-allow no; mirror --reverse --delete --only-newer $WDEFAULT_LOCAL_DIR $WDEFAULT_REMOTE_DIR; quit"
-
-fi
-
 
 echo "FTP Deploy Complete"
