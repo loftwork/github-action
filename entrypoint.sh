d7195c90f5faa4efd41bee0e1a486c34f99b9aa7
makotoomori <makoto@loftwork.com>
2019-09-11 04:20:08 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,14 @@ echo "Uploading files..."
 WDEFAULT_LOCAL_DIR=${LOCAL_DIR:-"."}
 WDEFAULT_REMOTE_DIR=${REMOTE_DIR:-"."}
 
-    echo "master branch -> PROD_SERVER..."
-    set +e
-    git config git-ftp.url $FTP_DEST
-    git config git-ftp.user $FTP_USER
-    git config git-ftp.password $FTP_PWD
+echo "master branch -> PROD_SERVER..."
+set +e
+
+echo "FTP_DEST=${FTP_DEST} FTP_USER=${FTP_USER} FTP_PWD=${FTP_PWD}"
+
+git config git-ftp.url $FTP_DEST
+git config git-ftp.user $FTP_USER
+git config git-ftp.password $FTP_PWD
     
     git ftp init
     exit_code=$?
