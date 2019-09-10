b9bb65dd3f480f74ac96f33097d099f80e7ff1f7
makotoomori <makoto@loftwork.com>
2019-09-11 04:39:36 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,20 @@ set -eu
 echo "Starting GIT-FTP Deploy"
 echo "Uploading files..."
 
-WDEFAULT_LOCAL_DIR=${LOCAL_DIR:-"."}
-WDEFAULT_REMOTE_DIR=${REMOTE_DIR:-"."}
-
 echo "master branch -> PROD_SERVER..."
 set +e
 
 echo "FTP_DEST=${FTP_DEST} FTP_USER=${FTP_USER} FTP_PWD=${FTP_PWD}"
+if [ $KEY != "" ]; then
+    echo $KEY > ./id_rsa
+fi
 
-git config git-ftp.url $FTP_DEST
+git config git-ftp.url "sftp://ftp-test.loftwork.work/var/www/vhosts/ftp-test/"
+
 git config git-ftp.user $FTP_USER
-git config git-ftp.password $FTP_PWD
-    
+#git config git-ftp.password $FTP_PWD
+git config git-ftp.key ./id_rsa
+
     git ftp init
     exit_code=$?
     if [ "$exit_code" -ne 0 ]; then
